594bcbda11b3133008e01f93f9e907e322a91c53
surekananth <dilip@hotmail.fr>
2016-10-18 17:32:39 +0200

    View and api tamil yogi

--- plugin.video.tamilstreamer/addon.py
+++ plugin.video.tamilstreamer/addon.py
@@ -1,17 +1,62 @@
 from xbmcswift2 import Plugin
-
+from resources.lib import api_tamilyogi as api_tamilyogi
 
 plugin = Plugin()
 
 
+# SITE VIEW
 @plugin.route('/')
 def index():
-    item = {
-        'label': 'Hello XBMC3!',
-        'path': 'http://s3.amazonaws.com/KA-youtube-converted/JwO_25S_eWE.mp4/JwO_25S_eWE.mp4',
-        'is_playable': True
-    }
-    return [item]
+    items = [
+        {'label': 'Tamil Yogi',
+         'path': plugin.url_for('section_view', site_name='tamilyogi')},
+
+        {'label': 'Tamil Gun',
+         'path': plugin.url_for('section_view', site_name='tamilgun')},
+    ]
+    return items
+
+
+# SECTION VIEW
+@plugin.route('/sections/<site_name>')
+def section_view(site_name):
+    if site_name == 'tamilyogi':
+        api = api_tamilyogi.TamilYogi()
+        sections = api.get_sections()
+
+    items = [{
+                 'label': section['name'],
+                 'path': plugin.url_for('movies_view', site_name=site_name, url=section['url']),
+             } for section in sections]
+
+    return items
+
+
+# MOVIES VIEW
+@plugin.route('/movies/<site_name>/<url>')
+def movies_view(site_name, url):
+    if site_name == 'tamilyogi':
+        api = api_tamilyogi.TamilYogi()
+        print url
+        movies = api.get_movies(url)
+
+    items = [{
+                 'label': movie['name'],
+                 'path': movie['url'],
+                'is_playable': True
+             } for movie in movies]
+
+    return items
+
+
+#@plugin.route('/lectures/<url>/')
+#def play_lecture(url):
+#    item = {'label': 'Movie',
+#            'path': url,
+#            'is_playable': True
+#            }
+
+#   return [item]
 
 
 if __name__ == '__main__':
